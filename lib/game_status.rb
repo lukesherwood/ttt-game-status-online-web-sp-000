8a46bdc36ab9fd161db2af26036391e19eacb1ca
Luke Sherwood <luke.sherwood55@gmail.com>
2019-12-12 04:06:58 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -28,6 +28,6 @@ def full?(board)
 end
 
 def draw?(board)
-  return false if full?(board) == true && won?(board) == true 
-else return true
+  return true if full?(board) != true && won?(board) != true 
+
 end
